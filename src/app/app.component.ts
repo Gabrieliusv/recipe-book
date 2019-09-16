f3bd2474b87200eceb0ad20466511076ab0918ab
Gabrieliusv <gabrielius.svi@gmail.com>
2019-09-16 22:14:12 +0300

    Added form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  navigation = 'recipe';
-
-  onNavigate(feature: string) {
-    this.navigation = feature;
-  }
-}
+export class AppComponent {}
